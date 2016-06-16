ebdd72303748358d908b96273ca2621ce724ca90
Lars Trieloff <lars@trieloff.net>
2016-06-16 08:03:46 +0200

    More robust escaping of spaces

--- meetup.sh
+++ meetup.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
-for topic in $(cat topics.txt)
+for topic in $(cat topics.txt | sed -e "s/ /%20/g")
 do
-  TOPIC=$(echo $topic | sed -e "s/ /%20/g")
+  TOPIC=$(echo $topic)
   JSON=$(curl "https://api.meetup.com/find/topics?photo-host=public&page=1&query=$TOPIC&key=$1" 2>/dev/null)
   TOPIC_ID=$(echo $JSON | jq ".[0].id")
   TOPIC_NAME=$(echo $JSON | jq -r ".[0].name")
   #echo "$TOPIC_NAME $TOPIC_ID"
-  for location in $(cat locations.txt)
+  for location in $(cat locations.txt | sed -e "s/ /%20/g")
   do
-    LOCATION=$(echo $location | sed -e "s/ /%20/g")
-    #echo "Getting $TOPIC_NAME Meetups in $location"
+    LOCATION=$(echo $location)
+    #echo "Getting $TOPIC_NAME Meetups in $LOCATION"
     #echo curl "https://api.meetup.com/find/groups?location=$location&radius=1&topic_id=$TOPIC_ID&order=members&key=$1&page=1"
-    MEETUP_URL=$(curl "https://api.meetup.com/find/groups?location=$location&radius=1&topic_id=$TOPIC_ID&order=members&key=$1&page=1" 2>/dev/null | jq -r ".[0].link")
+    MEETUP_URL=$(curl "https://api.meetup.com/find/groups?location=$LOCATION&radius=1&topic_id=$TOPIC_ID&order=members&key=$1&page=1" 2>/dev/null | jq -r ".[0].link")
     if [[ "$MEETUP_URL" != "null" ]]
     then
       MEETUP_NAME=$(curl "https://api.meetup.com/find/groups?location=$LOCATION&radius=1&topic_id=$TOPIC_ID&order=members&key=$1&page=1" 2>/dev/null | jq -r ".[0].name")
@@ -19,5 +19,6 @@ do
       MEETUP_MEMBERS=$(curl "https://api.meetup.com/find/groups?location=$LOCATION&radius=1&topic_id=$TOPIC_ID&order=members&key=$1&page=1" 2>/dev/null | jq -r ".[0].members")
       echo "$MEETUP_NAME\t$MEETUP_CITY\t$MEETUP_MEMBERS\t$MEETUP_URL\t$topic"
     fi
+    exit
   done
 done
