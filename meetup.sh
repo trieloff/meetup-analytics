7ea0415663ee4662f91ca0077cc92ace875c3bcd
Lars Trieloff <lars@trieloff.net>
2016-06-16 07:53:15 +0200

    Inital commit

---
+++ meetup.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+for topic in $(cat topics.txt)
+do
+  TOPIC=$(echo $topic | sed -e "s/ /%20/g")
+  JSON=$(curl "https://api.meetup.com/find/topics?photo-host=public&page=1&query=$TOPIC&key=$1" 2>/dev/null)
+  TOPIC_ID=$(echo $JSON | jq ".[0].id")
+  TOPIC_NAME=$(echo $JSON | jq -r ".[0].name")
+  #echo "$TOPIC_NAME $TOPIC_ID"
+  for location in $(cat locations.txt)
+  do
+    LOCATION=$(echo $location | sed -e "s/ /%20/g")
+    #echo "Getting $TOPIC_NAME Meetups in $location"
+    #echo curl "https://api.meetup.com/find/groups?location=$location&radius=1&topic_id=$TOPIC_ID&order=members&key=$1&page=1"
+    MEETUP_URL=$(curl "https://api.meetup.com/find/groups?location=$location&radius=1&topic_id=$TOPIC_ID&order=members&key=$1&page=1" 2>/dev/null | jq -r ".[0].link")
+    if [[ "$MEETUP_URL" != "null" ]]
+    then
+      MEETUP_NAME=$(curl "https://api.meetup.com/find/groups?location=$LOCATION&radius=1&topic_id=$TOPIC_ID&order=members&key=$1&page=1" 2>/dev/null | jq -r ".[0].name")
+      MEETUP_CITY=$(curl "https://api.meetup.com/find/groups?location=$LOCATION&radius=1&topic_id=$TOPIC_ID&order=members&key=$1&page=1" 2>/dev/null | jq -r ".[0].city")
+      MEETUP_MEMBERS=$(curl "https://api.meetup.com/find/groups?location=$LOCATION&radius=1&topic_id=$TOPIC_ID&order=members&key=$1&page=1" 2>/dev/null | jq -r ".[0].members")
+      echo "$MEETUP_NAME\t$MEETUP_CITY\t$MEETUP_MEMBERS\t$MEETUP_URL\t$topic"
+    fi
+  done
+done
